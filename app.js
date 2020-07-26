1dd27fc576cd0579b06693abc783c4bb0941cdf7
DaSilva123 <mateusma1999@gmail.com>
2020-07-26 14:51:54 -0300

    criando a API

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/teste',(req, res, next) => {
+    res.status(200).send({
+        mensagem: 'OK, Deu certo'
+    });
+});
+ 
+module.exports = app;
