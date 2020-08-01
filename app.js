549430028da78b46fe61f8eb5526aecd40cc1c8e
DaSilva123 <mateusma1999@gmail.com>
2020-08-01 12:44:34 -0300

    Tratando o CORS

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json()); 
 
+app.use((req, res, next) =>{
+    res.header('Acces-Control-Allow-Origin', '#');
+    res.header(
+        'Acces-Control-Allow-Header',
+        'Origin, Content-Type, X-Requrested-with, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+});
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
