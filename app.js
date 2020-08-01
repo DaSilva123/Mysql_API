5ec02390950811c4637d6fa653fb551305fde6e8
DaSilva123 <mateusma1999@gmail.com>
2020-08-01 11:43:07 -0300

    Tratanto erro passados por parametro

--- app.js
+++ app.js
@@ -9,4 +9,18 @@ app.use(morgan('dev'));
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+ app.use((req, res, next)=>{
+    const erro = new Error('não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 module.exports = app;
